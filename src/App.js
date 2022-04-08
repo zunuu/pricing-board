9d89b865d8e14cb5257da28dcdd3537468267482
zunuu <zunayedhossain99@gmail.com>
2022-04-09 02:06:07 +0600

    toggle sign in/ sign out

--- src/App.js
+++ src/App.js
@@ -2,33 +2,59 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Pricing from './Components/Pricing/Pricing';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { Result } from 'postcss';
 import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
-  const [userInfo, setUserInfo] = useState({})
+  const [userInfo, setUserInfo] = useState({});
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then(result => {
+        setUserInfo(result.user)
         console.log(result.user);
       })
       .catch(error => {
         console.log('i am error', error);
       })
   }
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUserInfo({});
+
+      })
+      .catch((error) => {
+        setUserInfo({});
+
+      });
+
+  }
   return (
     <div className="App">
       <Header></Header>
       <h1 className='text-6xl py-4 pb-0'>Welcome to my pricing club</h1>
       <Pricing></Pricing>
-      <button onClick={handleGoogleSignIn} className='border-0 bg-indigo-400 px-5 py-3 my-10 rounded-2xl'>Sign In</button>
-      <div className='bg-indigo-300 p-5'>
-        <h2>User Name:</h2>
-        <h5>email:</h5>
+
+      <div>
+        {
+          userInfo.email ?
+            <button onClick={handleGoogleSignOut} className='border-0 bg-indigo-400 px-5 py-3 my-10 rounded-2xl'>Sign Out</button>
+
+            :
+
+            <button onClick={handleGoogleSignIn} className='border-0 bg-indigo-300 px-5 py-3 my-10 rounded-2xl'>Sign In</button>
+
+        }
+      </div>
+
+
+      <div className='bg-indigo-300 p-5 font-bold'>
+        <h2>Username: {userInfo.displayName}</h2>
+        <h5>email: {userInfo.email}</h5>
       </div>
     </div>
   );
